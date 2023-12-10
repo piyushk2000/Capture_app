f212e0f47aa6c4ce7c3960fe80acfee374705dd9
Piyush <piyush.kalyani7@gmail.com>
2023-12-10 15:43:05 +0530

    cors changes

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+app.use(cors({
+  origin: '*',
+}));
 app.use(express.static(CLIENT_DIR));
 
 
